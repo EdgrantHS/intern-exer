3256957ebfe474229cf42123aa45a9de489cca90
EdgrantHS <edgrant.h@gmail.com>
2024-10-19 12:15:09 +0700

    Ganti width selamat datang

--- src/Sections/SelamatDatang.tsx
+++ src/Sections/SelamatDatang.tsx
@@ -2,13 +2,13 @@ import TeamBox from "../Components/TeamBox";
 
 export default function SelamatDatang() {
   return (
-    <div className="bg-white w-screen px-12">
+    <div className="bg-white w-screen md:w-[1500px] px-12">
       <h1 className="text-4xl font-bold h-fit w-fit py-4 px-8 text-primary bg-tertiary rounded-full drop-shadow text-center mt-16">
         SELAMAT DATANG DI INTERNSHIP FE EXERCISE 🎊🎊🥳🥳
       </h1>
       <section
         id="team-boxes"
-        className="flex gap-8 py-8 overflow-x-auto scrollbar-hide hover:scale-[101%] transition-all duration-300 ease-in-out overflow-y-visible
+        className="flex gap-8 py-8 overflow-x-auto hover:scale-[101%] transition-all duration-300 ease-in-out overflow-y-visible
         w-full md:w-[1432px] h-[400px] md:h-[500px] mt-4 md:mt-0 no-scrollbar"
       >
         <TeamBox
